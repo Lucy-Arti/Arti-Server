03a2d2e699ff1f45a168f10c41a815e4eb994ba4
devrokket <rokany@cau.ac.kr>
2023-09-07 22:17:14 +0900

    setting:first commit

---
+++ src/main/java/com/lucy/arti/ArtiApplication.java
@@ -0,0 +1,13 @@
+package com.lucy.arti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucy/arti/ArtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucy.arti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
