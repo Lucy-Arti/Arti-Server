8ef5328b1ec5a840df74f92c4714a25b41ed5728
devrokket <rokany@cau.ac.kr>
2023-09-11 19:27:51 +0900

    chore #5 : 기본 로그인 페이지 삭제

--- src/main/java/com/lucy/arti/ArtiApplication.java
+++ src/main/java/com/lucy/arti/ArtiApplication.java
@@ -2,8 +2,9 @@ package com.lucy.arti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // 로그인 페이지 생략
 public class ArtiApplication {
 
 	public static void main(String[] args) {
