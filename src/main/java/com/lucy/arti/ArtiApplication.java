e53f84483c82d6aaa4fe05e8ac04b45b285e87b6
math0917 <math0917@naver.com>
2023-09-11 19:56:03 +0900

    ARTI-44 변경사항 추가

--- src/main/java/com/lucy/arti/ArtiApplication.java
+++ src/main/java/com/lucy/arti/ArtiApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArtiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("원표 바보");
 		SpringApplication.run(ArtiApplication.class, args);
 	}
-
+	
 }
