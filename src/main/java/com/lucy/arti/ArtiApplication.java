aafe1d230cad7c0573f4e6072fcc8e10a86e458a
math0917 <math0917@naver.com>
2023-09-13 19:34:24 +0900

    ARTI-75 옷 entity 생성

--- src/main/java/com/lucy/arti/ArtiApplication.java
+++ src/main/java/com/lucy/arti/ArtiApplication.java
@@ -2,12 +2,12 @@ package com.lucy.arti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 
 @SpringBootApplication
 public class ArtiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ArtiApplication.class, args);
 	}
-	
 }
---
+++ src/main/java/com/lucy/arti/config/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.lucy.arti.config;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseTimeEntity {
+
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+}
