48226dd3d0c159ff9256a7e4a4c769ced4bb91c0
math0917 <math0917@naver.com>
2023-09-13 19:42:18 +0900

    ARTI-75 멤버 entity 생성

--- src/main/java/com/lucy/arti/member/model/Member.java
+++ src/main/java/com/lucy/arti/member/model/Member.java
@@ -1,4 +1,25 @@
 package com.lucy.arti.member.model;
 
-public class Member {
+import com.lucy.arti.config.BaseTimeEntity;
+import com.lucy.arti.config.Gender;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String userName;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
 }
