cc29f48b65005523910e654d52bf29b283013099
math0917 <math0917@naver.com>
2023-09-13 19:38:43 +0900

    ARTI-75 디자이너 entity 생성

--- src/main/java/com/lucy/arti/designer/domain/Designer.java
+++ src/main/java/com/lucy/arti/designer/domain/Designer.java
@@ -1,4 +1,27 @@
 package com.lucy.arti.designer.domain;
 
-public class Designer {
+import com.lucy.arti.config.BaseTimeEntity;
+import com.lucy.arti.config.Gender;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Designer extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "designer_id")
+    private Long id;
+
+    private String userName;
+
+    private String phoneNumber;
+
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
+
+    private String introduce;
+
 }
