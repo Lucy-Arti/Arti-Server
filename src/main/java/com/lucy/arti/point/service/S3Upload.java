4697ebb612b580c3d00ffaf348a39667d606425a
eunsu02 <regulus_leo08@naver.com>
2023-11-24 14:18:21 +0900

    [Fix] 주석 삭제

--- src/main/java/com/lucy/arti/point/service/S3Upload.java
+++ src/main/java/com/lucy/arti/point/service/S3Upload.java
@@ -12,30 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
-//@RequiredArgsConstructor
-//@Service
-//public class S3Upload {
-//
-//    @Value("${cloud.aws.s3.bucket}")
-//    private String bucket;
-//
-//    private final AmazonS3 amazonS3;
-//
-//    public String upload(MultipartFile multipartFile) throws IOException {
-//        String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
-//
-//        try (InputStream inputStream = multipartFile.getInputStream()) {
-//            ObjectMetadata objMeta = new ObjectMetadata();
-//            objMeta.setContentLength(multipartFile.getSize());
-//            amazonS3.putObject(bucket, s3FileName, inputStream, objMeta);
-//        } catch (AmazonS3Exception e) {
-//            // S3 업로드 중 오류가 발생한 경우 예외 처리
-//            throw new RuntimeException("S3 업로드 중 오류가 발생했습니다.", e);
-//        }
-//
-//        return amazonS3.getUrl(bucket, s3FileName).toString();
-//    }
-//}
 @RequiredArgsConstructor
 @Service
 public class S3Upload {
