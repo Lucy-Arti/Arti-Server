944ebcf0f191fdcfadd4efb6ae5cda2ec3270e12
eunsu02 <regulus_leo08@naver.com>
2023-11-24 14:03:55 +0900

    [Fix] s3 upload 오류 삭제

---
+++ src/main/java/com/lucy/arti/point/service/S3Upload.java
@@ -0,0 +1,64 @@
+package com.lucy.arti.point.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+//@RequiredArgsConstructor
+//@Service
+//public class S3Upload {
+//
+//    @Value("${cloud.aws.s3.bucket}")
+//    private String bucket;
+//
+//    private final AmazonS3 amazonS3;
+//
+//    public String upload(MultipartFile multipartFile) throws IOException {
+//        String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+//
+//        try (InputStream inputStream = multipartFile.getInputStream()) {
+//            ObjectMetadata objMeta = new ObjectMetadata();
+//            objMeta.setContentLength(multipartFile.getSize());
+//            amazonS3.putObject(bucket, s3FileName, inputStream, objMeta);
+//        } catch (AmazonS3Exception e) {
+//            // S3 업로드 중 오류가 발생한 경우 예외 처리
+//            throw new RuntimeException("S3 업로드 중 오류가 발생했습니다.", e);
+//        }
+//
+//        return amazonS3.getUrl(bucket, s3FileName).toString();
+//    }
+//}
+@RequiredArgsConstructor
+@Service
+public class S3Upload {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private static final String S3_PATH_PREFIX = "instagram-image/";
+
+    private final AmazonS3 amazonS3;
+
+    public String upload(MultipartFile multipartFile) throws IOException {
+        String s3FileName = S3_PATH_PREFIX + UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            ObjectMetadata objMeta = new ObjectMetadata();
+            objMeta.setContentLength(multipartFile.getSize());
+
+            amazonS3.putObject(bucket, s3FileName, inputStream, objMeta);
+        } catch (AmazonS3Exception e) {
+            // S3 업로드 중 오류가 발생한 경우 예외 처리
+            throw new RuntimeException("S3 업로드 중 오류가 발생했습니다.", e);
+        }
+        return amazonS3.getUrl(bucket, s3FileName).toString();
+    }
+}
