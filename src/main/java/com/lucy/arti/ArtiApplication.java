23b9ffd8ea06354a3d53ec68e3d46c2e687b3f80
eunsu02 <regulus_leo08@naver.com>
2023-11-24 20:45:52 +0900

    [Fix] 스케쥴링 작동 오류

--- src/main/java/com/lucy/arti/ArtiApplication.java
+++ src/main/java/com/lucy/arti/ArtiApplication.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
+import java.util.TimeZone;
+
 @EnableScheduling //스케쥴링 사용
 @EnableJpaAuditing
 @SpringBootApplication
 public class ArtiApplication {
-	public static void main(String[] args) { SpringApplication.run(ArtiApplication.class, args);}
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		SpringApplication.run(ArtiApplication.class, args);}
 }
