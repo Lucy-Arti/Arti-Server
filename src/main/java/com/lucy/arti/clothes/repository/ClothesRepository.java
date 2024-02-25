55f91bb44d3a7fe74e5dc6481d3223f8d38cb8ec
devrokket <rokany@cau.ac.kr>
2024-02-25 11:05:35 +0900

    refactor: Pageable 객체를 입력받아 Page<Clothes> 타입 객체를 리턴하도록 findAllBy

--- src/main/java/com/lucy/arti/clothes/repository/ClothesRepository.java
+++ src/main/java/com/lucy/arti/clothes/repository/ClothesRepository.java
@@ -2,25 +2,27 @@ package com.lucy.arti.clothes.repository;
 
 import com.lucy.arti.clothes.domain.Clothes;
 import com.lucy.arti.clothes.domain.Type;
-import com.lucy.arti.clothes.dto.ClothesDetailResponseDto;
-import com.lucy.arti.designer.domain.Designer;
 import com.lucy.arti.global.exception.BusinessException;
 import com.lucy.arti.global.exception.ErrorCode;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+// 페이지네이션에 사용
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 
 public interface ClothesRepository extends JpaRepository<Clothes, Long> {
+
     @Query("SELECT c FROM Clothes c WHERE " +
-            "c.name LIKE CONCAT('%',:query, '%')" +
-            "Or c.designer.userName LIKE CONCAT('%', :query, '%')")
+        "c.name LIKE CONCAT('%',:query, '%')" +
+        "Or c.designer.userName LIKE CONCAT('%', :query, '%')")
     List<Clothes> searchClothes(String query);
 
     List<Clothes> findByDesignerId(Long designerId);
 
-    List<Clothes> findAllByType(Type type);
+    Page<Clothes> findAllByType(Type type, Pageable pageable);
 
     default Clothes findByIdOrThrow(Long id) {
         return findById(id).orElseThrow(
